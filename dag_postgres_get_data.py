0b95a1068c4e8c3ac9f8202e2379f930e36f0284
Dima Kyrilkov <dima.kapkan@gmail.com>
2021-08-22 15:53:14 +0300

    add dag

--- dag_postgres_get_data.py
+++ dag_postgres_get_data.py
@@ -13,3 +13,17 @@ def get_tables_from_variables(key):
     return tables['dshop']
 
 
+default_args = {
+    'owner': 'airflow',
+    'email': ['airflow@airflow.com'],
+    'email_on_failure': False,
+    'retries': 1
+}
+
+dag = DAG(
+    dag_id='hw_14_postgres',
+    description='Home work for lesson 14',
+    schedule_interval='@daily',
+    start_date=datetime(2021, 8, 10, 23),
+    default_args=default_args
+)
