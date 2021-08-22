8c3b724c248071d44103440a955f9163c804cadb
Dima Kyrilkov <dima.kapkan@gmail.com>
2021-08-22 15:55:22 +0300

    get data from DB

--- dag_postgres_get_data.py
+++ dag_postgres_get_data.py
@@ -27,3 +27,16 @@ dag = DAG(
     start_date=datetime(2021, 8, 10, 23),
     default_args=default_args
 )
+
+
+for table in get_tables_from_variables('tables'):
+    get_data_to_bronze = PythonOperator(
+        task_id=f'get_{table}_from_dshop',
+        python_callable=get_table_data_for_file,
+        op_kwargs={
+            'connection_id': 'dshop',
+            'table': table,
+            'current_date': today
+        },
+        dag=dag
+    )
---
+++ get_data_from_db.py
@@ -0,0 +1,63 @@
+import sys
+
+from hdfs import InsecureClient
+import psycopg2
+import os
+import logging
+from airflow.hooks.base_hook import BaseHook
+from pyspark.sql import SparkSession
+
+
+def get_airflow_connections(connection_id):
+    connection = BaseHook.get_connection(connection_id)
+    logging.info(
+        f'Connection config: {connection.host}, DB {connection.schema}, Login {connection.login}, PWD {connection.password}')
+    creds = {
+        'host': connection.host,
+        'port': connection.port,
+        'database': connection.schema,
+        'user': connection.login,
+        'password': connection.password
+    }
+    return creds
+
+
+def get_table_data_for_file(**kwargs):
+    client = InsecureClient('http://127.0.0.1:50070/', user='user')
+    creds = get_airflow_connections(kwargs['connection_id'])
+    try:
+        connect = psycopg2.connect(**creds)
+        cursor = connect.cursor()
+        table_name = kwargs['table']
+        today = kwargs['current_date']
+        logging.info(f'Table: {table_name}')
+        with client.write(
+                os.path.join('bronze', today, kwargs['connection_id'],  f'{table_name}.csv'), overwrite=True
+        ) as csv:
+            cursor.copy_expert(f'COPY {table_name} to STDOUT WITH HEADER CSV', csv)
+        logging.info(f"File: {os.path.join('bronze', today, kwargs['connection_id'], f'{table_name}.csv')}")
+        cursor.close()
+        logging.info('*****success********')
+    except psycopg2.Error as e:
+        logging.error(e)
+        sys.exit(1)
+
+
+def bronze_to_silver(**kwargs):
+    try:
+        table_name = kwargs['table']
+        today = kwargs['current_date']
+        spark = SparkSession.builder.master('local').appName('hw_14').getOrCreate()
+
+        table_data_df = spark.read.load(os.path.join('bronze', today, kwargs['connection_id'], f'{table_name}.csv'),
+                                        header="true",
+                                        inferSchema="true",
+                                        format="csv")
+        table_data_df = table_data_df.dropDuplicates()
+        logging.info(f'{table_name}: duplicates delete')
+        table_data_df.write.parquet(os.path.join('silver', today, kwargs['connection_id'], table_name),
+                                    mode='overwrite')
+        logging.info(f'{table_name}.parquet successfully saved to SILVER')
+    except:
+        logging.error(f'There are problems with data processing, please see the execution logs')
+        sys.exit(1)
